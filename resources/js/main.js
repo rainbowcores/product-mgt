6632224f6010bb95d177f42dd5fdd0b37969592c
Christine Wanjiru <christine.wanjiru@mediapal.net>
2021-03-18 14:12:59 +0300

    set up front end templating

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,49 +1,22 @@
-/*!
-
- =========================================================
- * Vue Light Bootstrap Dashboard - v2.0.0 (Bootstrap 4)
- =========================================================
-
- * Product Page: http://www.creative-tim.com/product/light-bootstrap-dashboard
- * Copyright 2019 Creative Tim (http://www.creative-tim.com)
- * Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard/blob/master/LICENSE.md)
-
- =========================================================
-
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
- */
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'bootstrap-css-only/css/bootstrap.min.css'
+import 'mdbvue/lib/css/mdb.min.css'
 import Vue from 'vue'
-import VueRouter from 'vue-router'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+// import * as VueGoogleMaps from 'vue2-google-maps'
 
-// LightBootstrap plugin
-import LightBootstrap from './light-bootstrap-main'
+// Vue.use(VueGoogleMaps, {
+//   load: {
+//     libraries: 'places'
+//   }
+// })
 
-// router setup
-import routes from './routes/routes'
+Vue.config.productionTip = false
 
-import './registerServiceWorker'
-// plugin setup
-Vue.use(VueRouter)
-Vue.use(LightBootstrap)
-
-// configure router
-const router = new VueRouter({
-  routes, // short for routes: routes
-  linkActiveClass: 'nav-item active',
-  scrollBehavior: (to) => {
-    if (to.hash) {
-      return {selector: to.hash}
-    } else {
-      return { x: 0, y: 0 }
-    }
-  }
-})
-
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  render: h => h(App),
-  router
-})
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,8 +20,8 @@
             }
         </style>
     </head>
-    <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+    <body >
+        {{-- <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
@@ -127,6 +127,10 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> --}}
+        <div id="app">
+          </div>
+        
+          <script src="{{ asset('js/main.js') }}"></script>
     </body>
 </html>
